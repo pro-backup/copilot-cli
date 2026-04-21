660bb1e4b5271e55f0ff28e7fcb49328f0512499
Alexey Vilenski <vilenskialeksei@gmail.com>
2026-04-21 10:49:08 +0200

    Env CFN: stack outputs for IPv6; add ipv6-enabled golden
    Outputs IPv6Enabled, VpcIpv6CidrBlock, and per-subnet-tier IPv6 CIDR
    lists so downstream sub-projects (#2, #3, #5) can consume them via
    DescribeStacks. Adds template-with-ipv6-enabled.yml as the canonical
    dual-stack env render.

--- internal/pkg/deploy/cloudformation/stack/env_integration_test.go
+++ internal/pkg/deploy/cloudformation/stack/env_integration_test.go
@@ -263,6 +263,33 @@ network:
 			}(),
 			wantedFileName: "template-with-importedvpc-flowlogs.yml",
 		},
+		"ipv6 dual-stack enabled": {
+			input: func() *stack.EnvConfig {
+				rawMft := `name: test
+type: Environment
+network:
+  vpc:
+    ipv6:
+      enabled: true
+`
+				var mft manifest.Environment
+				err := yaml.Unmarshal([]byte(rawMft), &mft)
+				require.NoError(t, err)
+				return &stack.EnvConfig{
+					Version: "1.x",
+					App: deploy.AppInformation{
+						AccountPrincipalARN: "arn:aws:iam::000000000:root",
+						Name:                "demo",
+					},
+					Name:                 "test",
+					ArtifactBucketARN:    "arn:aws:s3:::mockbucket",
+					ArtifactBucketKeyARN: "arn:aws:kms:us-west-2:000000000:key/1234abcd-12ab-34cd-56ef-1234567890ab",
+					Mft:                  &mft,
+					RawMft:               rawMft,
+				}
+			}(),
+			wantedFileName: "template-with-ipv6-enabled.yml",
+		},
 	}
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
